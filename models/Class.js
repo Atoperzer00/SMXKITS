22e35bd6b72ab5ec26eb8cdce3c7cda324faf987
SMX KITS Developer <developer@smxkits.com>
2025-06-09 04:32:42 -0700

    Updated files and implemented requested changes

--- models/Class.js
+++ models/Class.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
 const ClassSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
+  organization: { type: String },
+  country: { type: String },
+  instructorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  startDate: { type: Date },
+  endDate: { type: Date },
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  instructors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  instructors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Class', ClassSchema);
